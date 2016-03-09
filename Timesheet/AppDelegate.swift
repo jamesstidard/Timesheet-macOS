1d4e23973d285271e9a68604bbcfec2b81be39d4
jamesstidard <jamesstidard@gmail.com>
2016-03-09 18:55:27 +0000

    Initial Commit

---
+++ Timesheet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timesheet
+//
+//  Created by James Stidard on 09/03/2016.
+//  Copyright © 2016 James Stidard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Timesheet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timesheet
+//
+//  Created by James Stidard on 09/03/2016.
+//  Copyright © 2016 James Stidard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
