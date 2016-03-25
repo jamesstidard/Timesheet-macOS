3f3f7b9a32f8c88c4930cf9ce31dcb771ec03faf
jamesstidard <jamesstidard@gmail.com>
2016-03-25 01:48:35 +0000

    init get projects

--- Timesheet/View Controllers/ViewController.swift
+++ Timesheet/View Controllers/ViewController.swift
@@ -16,14 +16,23 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var fuzzyInputTokenField: NSTokenField!
     
-    let projects: [Project] = []
+    var projects: [Project] = []
     let log = Log()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.fuzzyInputTokenField
+            .rac_textSignal()
+            .toSignalProducer()
+            .map {entry in entry as! String}
+            .flatMap(.Latest) {ViewController.requestProjects($0, limit: 10)}
+            .map(ViewController.makeProjects)
+            .observeOn(UIScheduler())
+            .startWithNext { projects in
+                self.projects = projects
+            }
     }
 
     override var representedObject: AnyObject? {
@@ -32,6 +41,24 @@ class ViewController: NSViewController {
         }
     }
 
-
+    static func requestProjects(query: String, limit: Int) -> ReactiveCocoa.SignalProducer<(NSData, NSURLResponse), NSError> {
+        let url     = NSURL(string: "\(self.BaseUrl)/projects?query=\(query)&limit=\(limit)")!
+        let request = NSMutableURLRequest(URL: url)
+        request.setValue(TokenID, forHTTPHeaderField: "token-id")
+        request.setValue(TokenSecret, forHTTPHeaderField: "token-secret")
+        
+        return NSURLSession.sharedSession().rac_dataWithRequest(request)
+    }
+    
+    static func makeProjects(data: NSData, _: NSURLResponse) -> [Project] {
+        guard let
+            json    = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions()) as! [String: AnyObject],
+            payload = ProjectPayload(json: json)
+        else {
+            return []
+        }
+        
+        return payload.result
+    }
 }
 
